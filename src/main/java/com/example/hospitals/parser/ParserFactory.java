e03e7bc0b65715968c6d3bb77c8eb0b6c16f17d6
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-31 15:41:04 +0900

    feat: ParserFactor 클래스 추가하여 AutoWired 사용하여 ReadLineContext 생성

--- src/main/java/com/example/hospitals/parser/ParserFactory.java
+++ src/main/java/com/example/hospitals/parser/ParserFactory.java
@@ -1,5 +1,14 @@
 package com.example.hospitals.parser;
 
+import com.example.hospitals.domain.Hospital;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ParserFactory {
 
+    @Bean
+    public ReadLineContext<Hospital> hospitalReadLineContext() {
+        return new ReadLineContext<Hospital>(new HospitalParser());
+    }
 }
--- src/test/java/com/example/hospitals/parser/HospitalParserTest.java
+++ src/test/java/com/example/hospitals/parser/HospitalParserTest.java
@@ -3,13 +3,30 @@ package com.example.hospitals.parser;
 import com.example.hospitals.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest // @ExtendWith 와 @ConfigurationContext 를 대체한다.
 class HospitalParserTest {
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\"";
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext; // hospitalReadLineContext 라는 이름을 가진 메서드를 @Configuration 클래스의 @Bean 메서드 중에서 찾는다.
+
+    @Test
+    void name() throws IOException {
+        // 이러한 파일 명은 서버 환경에서 실행 시 문제가 될 수 있지만, 코드 자체에 포함시키기에는 부담스럽다.
+        String filename = "C:\\LikeLion\\2022.10\\ing\\spring-boot\\fulldata_01_01_02_P_의원3.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readLines(filename);
+        System.out.println(hospitalList.size());
+        assertTrue(hospitalList.size() > 10000);
+        assertTrue(hospitalList.size() > 100000);
+    }
 
     @DisplayName("parse test")
     @Test

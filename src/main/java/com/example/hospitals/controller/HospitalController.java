832c3b9ba73423aa72b73533d1f683a5a9d690a0
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-31 15:05:20 +0900

    feat: HospitalController 에서 POST 요청을 할 수 있도록 변경, HospitalDao 에서 insertInfo 메서드 구현

--- src/main/java/com/example/hospitals/controller/HospitalController.java
+++ src/main/java/com/example/hospitals/controller/HospitalController.java
@@ -1,4 +1,22 @@
 package com.example.hospitals.controller;
 
+import com.example.hospitals.dao.HospitalDao;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hospital/v1/api")
 public class HospitalController {
+    private final HospitalDao hospitalDao;
+
+    public HospitalController(HospitalDao hospitalDao) {
+        this.hospitalDao = hospitalDao;
+    }
+
+    @PostMapping("/info")
+    public void insertInfo() {
+        hospitalDao.add();
+    }
 }
--- src/main/java/com/example/hospitals/dao/HospitalDao.java
+++ src/main/java/com/example/hospitals/dao/HospitalDao.java
@@ -1,5 +1,36 @@
 package com.example.hospitals.dao;
 
+import com.example.hospitals.domain.Hospital;
+import com.example.hospitals.parser.HospitalParser;
+import com.example.hospitals.parser.ReadLineContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class HospitalDao {
 
+    private JdbcTemplate jdbcTemplate;
+    private ReadLineContext<Hospital> readLineContext;
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.readLineContext = new ReadLineContext<>(new HospitalParser());
+    }
+
+    public void add() {
+        List<Hospital> hospitalList = readLineContext.readLines("C:\\LikeLion\\2022.10\\ing\\spring-boot\\fulldata_01_01_02_P_의원 (2).csv");
+
+        for (Hospital hospital : hospitalList) {
+            jdbcTemplate.update("INSERT INTO nation_wide_hospitals VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
+                    hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
+                    hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
+                    hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
+                    hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
+                    hospital.getTotalAreaSize());
+        }
+
+    }
 }

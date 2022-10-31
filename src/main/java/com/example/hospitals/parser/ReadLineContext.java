1ed4bb0beeca810d7700ac02397674985663a0af
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-31 15:40:29 +0900

    fix: 파일 읽어들일 때 UTF_16 인코딩 사용하도록 변경

--- src/main/java/com/example/hospitals/parser/ReadLineContext.java
+++ src/main/java/com/example/hospitals/parser/ReadLineContext.java
@@ -3,6 +3,7 @@ package com.example.hospitals.parser;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -16,14 +17,21 @@ public class ReadLineContext<T> {
         this.list = new ArrayList<>();
     }
 
-    public List<T> readLines(String filename) {
+    public List<T> readLines(String filename) throws IOException {
         String str = "";
+        BufferedReader reader;
         try {
-            BufferedReader reader = Files.newBufferedReader(Paths.get(filename));
-            while((str = reader.readLine()) != null) this.list.add(parser.parse(str));
+            reader = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_16);
+            reader.readLine();
+            while ((str = reader.readLine()) != null) try {
+                this.list.add(parser.parse(str));
+            } catch (Exception e) {
+                System.out.printf("파일 내용: %s 에 문제가 생겨 넘어갑니다.\n", str);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        reader.close();
         return this.list;
     }
 }
